18027ef0d983c1468320a8106c3f3bed3f1087b4
Urs Albisser <u.albisser@gmail.com>
2020-10-10 20:33:29 +0200

    init commit

---
+++ src/01_syntax/01_welcome.py
@@ -0,0 +1,10 @@
+"""
+ 01_welcome
+ 
+ @author  created by Urs Albisser, on 2020-09-22
+ @version 0.1
+"""
+
+my_name = "Urs"
+print("Hello and welcome " + my_name + "!")
+
---
+++ src/01_syntax/04_strings.py
@@ -0,0 +1,13 @@
+"""
+ 04_strings
+ 
+ @author  created by Urs Albisser, on 2020-10-10
+ @version 0.1
+"""
+
+# Strings
+#
+# Computer programmers refer to blocks of text as strings. In our last exercise, we created the string “Hello world!”. In Python a string is either surrounded by double quotes ("Hello world") or single quotes ('Hello world'). It doesn’t matter which kind you use, just be consistent.
+
+print("Urs")
+print('Urs')
